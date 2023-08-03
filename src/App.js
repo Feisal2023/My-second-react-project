ee671745d19eb22138ad45072f50bb3688be4704
Feisal2023 <feisaladan2022@gmail.com>
2023-08-03 09:30:30 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useState } from "react";
 function App() {
+  const [name, setName] = useState("");
+  const [loadName, setLoadName] = useState("");
+  const [age, setAge] = useState("");
+  const [loadAge, setLoadAge] = useState("");
+
+  // Get name function
+  const getName = (event) => {
+    setName(event.target.value);
+  };
+  // get age function
+  const getAge = (event) => {
+    setAge(event.target.value);
+  };
+  const paragraphContent = () => {
+    setLoadAge(
+      age === ""
+        ? "and Please Enter Your Age"
+        : age > 18
+        ? `and your age is ${age} and it is over 18`
+        : `and Your age is ${age} and it is under 18`
+    );
+
+    setLoadName(
+      name !== "" ? `Your Name is ${name}` : "please Enter Your Name"
+    );
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" placeholder="Enter Your Name" onChange={getName} />
+      <input type="number" placeholder="Enter Your Age" onChange={getAge} />
+      <button onClick={paragraphContent}>Click</button>
+      <p>
+        {loadName} {loadAge}
+      </p>
     </div>
   );
 }
